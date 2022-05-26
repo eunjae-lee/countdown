2495eaa5c42da5877e4e06af71e330b6ae7d7178
Eunjae Lee <hey@eunjae.dev>
2022-05-26 15:22:08 +0200

    add stuff

---
+++ src/lib/components/countdown.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let targetTime: Date;
+
+	let hours: number;
+	let minutes: number;
+	let seconds: number;
+
+	onMount(() => {
+		const id = setInterval(() => {
+			let totalSeconds = (targetTime.getTime() - new Date().getTime()) / 1000;
+			hours = Math.floor(totalSeconds / 60 / 60);
+
+			totalSeconds = totalSeconds % (60 * 60);
+			minutes = Math.floor(totalSeconds / 60);
+
+			totalSeconds = totalSeconds % 60;
+			seconds = totalSeconds;
+		}, 1000);
+	});
+</script>
+
+<div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+	<div class="flex flex-col">
+		<span class="countdown font-mono text-5xl">
+			<span style={`--value:${hours};`} />
+		</span>
+		hours
+	</div>
+	<div class="flex flex-col">
+		<span class="countdown font-mono text-5xl">
+			<span style={`--value:${minutes};`} />
+		</span>
+		min
+	</div>
+	<div class="flex flex-col">
+		<span class="countdown font-mono text-5xl">
+			<span style={`--value:${seconds};`} />
+		</span>
+		sec
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts" context="module">
+	export async function load({ url }) {
+		return {
+			props: {
+				givenTimeStr: url.searchParams.get('time')
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Countdown from '$lib/components/countdown.svelte';
+	import { onMount } from 'svelte';
+
+	export let givenTimeStr: string;
+	let targetTimeStr: string;
+	let targetTime: Date;
+
+	function setTimeStr(timeStr: string) {
+		if (!timeStr) {
+			return;
+		}
+		const [hour, minute] = timeStr.split(':');
+		const now = new Date();
+		now.setHours(parseInt(hour, 10));
+		now.setMinutes(parseInt(minute, 10));
+
+		targetTime = now;
+	}
+
+	onMount(() => {
+		if (givenTimeStr) {
+			setTimeStr(givenTimeStr);
+		}
+	});
+</script>
+
+<div class="h-screen">
+	<div class="flex flex-col gap-4 h-full items-center justify-center">
+		{#if targetTime === undefined}
+			<p class="text-lg font-bold">Enter countdown time:</p>
+			<input bind:value={targetTimeStr} type="time" class="text-xl input input-bordered" />
+			<button type="button" class="btn" on:click={() => setTimeStr(targetTimeStr)}>Show</button>
+		{:else}
+			<Countdown {targetTime} />
+		{/if}
+	</div>
+</div>
