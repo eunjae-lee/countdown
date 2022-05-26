6c85914614d7431efdb07cf430c3a1b0ae48182a
Eunjae Lee <hey@eunjae.dev>
2022-05-26 15:33:58 +0200

    provide dedicated path

---
+++ src/routes/[slug].svelte
@@ -0,0 +1,33 @@
+<script lang="ts" context="module">
+	function parseTimeStr(timeStr: string) {
+		if (!timeStr) {
+			return null;
+		}
+		const [hour, minute] = timeStr.split(':');
+		const date = new Date();
+		date.setHours(parseInt(hour, 10));
+		date.setMinutes(parseInt(minute, 10));
+
+		return date;
+	}
+
+	export async function load({ params }) {
+		return {
+			props: {
+				targetTime: parseTimeStr(params.slug)
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import Countdown from '$lib/components/countdown.svelte';
+
+	export let targetTime: Date;
+</script>
+
+<div class="h-screen">
+	<div class="flex flex-col gap-4 h-full items-center justify-center">
+		<Countdown {targetTime} />
+	</div>
+</div>
